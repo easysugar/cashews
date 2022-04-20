6f94352785948190d6c0bdfaead5236b1982d51f
Krukov D <Glebov.ru@gmail.com>
2022-04-20 23:15:18 +0300

    Update article.py

--- examples/article.py
+++ examples/article.py
@@ -42,3 +42,30 @@ def cache(function):
         return result
     return _wrapped
  
+    
+import asyncio
+from functools import wraps
+from datetime import timedelta
+
+def cache(key_function: Callable, ttl: timedelta):
+    def _decorator(function):
+        @wraps(function)
+        async def _function(*args, **kwargs):
+            cache_key = key_function(*args, **kwargs)
+            result = await _cache.get(cache_key)
+            if result:
+                return result
+            result = await function(*args, **kwargs)
+            asyncio.create_task(_cache.set(cache_key, result, ttl=ttl.total_seconds()))
+            return result
+        return _function
+    return _decorator
+
+
+def _key_function(user, account_id):
+    return f"user-account-{user.id}-{account_id}"
+
+
+@cache(_key_function, timedelta(hours=3))
+async def get_user_account(*, user: User, account_id: int):
+    ...
